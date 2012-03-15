01524618f55df92b9ef720827b54b625af207cc5
lyon <lyondhill@gmail.com>
2012-03-15 16:50:41 -0600

    ansel

--- watch.php
+++ watch.php
@@ -3,13 +3,12 @@
 require 'lib/Resque.php';
 Resque::setBackend('tunnel.pagodabox.com:6379');
 
-$status = new Resque_Job_Status($argv[1]);
+$status = new Resque_Job_Status($_GET['code']);
 if(!$status->isTracking()) {
   echo "Resque is not tracking the status of this job.\n";
 } else {
   echo "Tracking status of ".$argv[1].". Press [break] to stop.\n\n";
-  echo "Status of ".$_GET['code']." is: ".$status->get()."\n");
-
+  echo "Status of ".$_GET['code']." is: ".$status->get()."\n";
 }
 ?>
 

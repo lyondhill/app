6c4872052afbbd1536f4c92a21bf470e9d4764ca
lyon <lyondhill@gmail.com>
2012-03-15 21:28:29 -0600

    what the craw

--- worker.php
+++ worker.php
@@ -8,6 +8,7 @@ class Send_Email
     public function perform()
     {
         // Work work work
+        mail('lyondhill@gmail.com', 'Test Subject', "This is an email message from the pongo app");
         echo $this->args['name'];
         echo "email sent";
     }

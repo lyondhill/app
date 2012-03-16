95c3e25017990c2fb428a9f2de277fe867c7ebcd
Tyler Flint <tylerflint@gmail.com>
2012-03-15 19:03:35 -0600

    reverting the worker just to test that it will actually work

--- worker.php
+++ worker.php
@@ -1,19 +1,28 @@
 <?php
 
-require 'lib/Resque.php';
-Resque::setBackend('tunnel.pagodabox.com:6379');
+// require 'lib/Resque.php';
+// Resque::setBackend('tunnel.pagodabox.com:6379');
 
-class Send_Email
-{
-    public function perform()
-    {
-        // Work work work
-        mail('lyondhill@gmail.com', 'Test Subject', "This is an email message from the pongo app");
-        echo $this->args['name'];
-        echo "email sent";
-    }
+// class Send_Email
+// {
+//     public function perform()
+//     {
+//         // Work work work
+//         mail('lyondhill@gmail.com', 'Test Subject', "This is an email message from the pongo app");
+//         echo $this->args['name'];
+//         echo "email sent";
+//     }
+// }
+
+// require 'resque.php';
+
+echo "hello there I'm starting up, woot!";
+
+while (true) {
+  
+  echo "loop da loop";
+  sleep(5);
+
 }
-
-require 'resque.php';
 
 ?>
